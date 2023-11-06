365429031e2f2613d1662cf90d6487ea0e5f7a54
nguyenhuudung201 <115616484+nguyenhuudung201@users.noreply.github.com>
2023-11-06 11:16:21 +0700

    first commit

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -20,3 +20,14 @@ exports.getAll = async function (req, res) {
   const products = await productModel.find();
   res.json(products);
 };
+
+exports.deleteById = async (req, res) => {
+  // Check if product with this id exists?
+  const id = req.params.id;
+  const result = await productModel.findById(id).exec();
+  // If not, return not found
+  if (!result) res.status(404).send("Product not found.");
+  // Else, delete then return Ok
+  await productModel.findByIdAndDelete(id);
+  res.status(200).send("Xoa thanh cong");
+};
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -5,4 +5,5 @@ const controller = require("./../controllers/ProductController");
 router.get("/", controller.home);
 router.post("/add", controller.add);
 router.get("/api/all", controller.getAll);
+router.get("/delete/:id", controller.deleteById);
 module.exports = router;
